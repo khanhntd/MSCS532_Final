f1a67cc52696a7ed63ca581b1488117978689aa6
Kel Nguyen <khanhntd@amazon.com>
2024-09-22 16:18:49 -0700

    Add comments for key operations

--- social_net.py
+++ social_net.py
@@ -1,5 +1,4 @@
 import pickle
-import operator
 import networkx as nx
 import matplotlib.pyplot as plt
 from arcplot import *
@@ -8,38 +7,38 @@ from itertools import combinations
 from dataset import downloadDataset
 
 class SocialNetwork:
-  def __init__(self, githubGraph: nx.Graph) -> None:
-    self.githubGraph = githubGraph
+  def __init__(self, fbGraph: nx.Graph) -> None:
+    self.fbGraph = fbGraph
 
   def drawGraph(self, degreeDistribution: bool = False) -> None:
     plt.figure(figsize=(8, 8))
     if degreeDistribution:
-      plt.title(label="Github degree centrality")
-      plt.hist(list(nx.degree_centrality(self.githubGraph).values()))
+      plt.title(label="Facebook degree centrality")
+      plt.hist(list(nx.degree_centrality(self.fbGraph).values()))
     else:
-      plt.title(label="Github Network")
-      nx.draw(self.githubGraph, with_labels=True)
+      plt.title(label="Facebook Network")
+      nx.draw(self.fbGraph, with_labels=True)
     plt.show(block = False)
     plt.pause(5)
 
-  # findImportantCollaborators will use degree centrality to determine the most important Github Developers
-  # that works on the same project within a repo together
-  def findImportantCollaborators(self) -> list[tuple]:
-    degCent = nx.degree_centrality(self.githubGraph)
+  # findImportantPeople will use degree centrality to determine the most important Facebook person
+  # that have many friends or being followed by others facebook user
+  def findImportantPeople(self) -> list[tuple]:
+    degCent = nx.degree_centrality(self.fbGraph)
     degCent = {k: v for k, v in sorted(degCent.items(), key=lambda item: item[1], reverse=True)}
     maxDegCent = max(list(degCent.values()))
-    importantCollaborators = [(n, dc) for n, dc in degCent.items() if dc == maxDegCent]
-    return importantCollaborators
+    importantPeople = [(n, dc) for n, dc in degCent.items() if dc == maxDegCent]
+    return importantPeople
 
   # connectCommunities will find the two largest communities based on the cliques and
-  # determine the bottleneck user based on their collaborations or followers
-  # Afterwards, if there is a path or similar interest between them, we can
-  # recommend the repository for the followers that those bottleneck users work at
+  # determine the bottleneck user based on the number of friends or followers
+  # they have. Afterwards, if there is a path or similar interest between them, we can
+  # recommend the bottle neck's user to other followers/friends
   # to increase the reach
   def connectCommunities(self):
-    cliques = sorted(nx.find_cliques(self.githubGraph), key=lambda x: len(x))
-    largestClique = self.githubGraph.subgraph(set(cliques[-1])).copy()
-    secondLargestClique = self.githubGraph.subgraph(set(cliques[-2])).copy()
+    cliques = sorted(nx.find_cliques(self.fbGraph), key=lambda x: len(x))
+    largestClique = self.fbGraph.subgraph(set(cliques[-1])).copy()
+    secondLargestClique = self.fbGraph.subgraph(set(cliques[-2])).copy()
     largestCliqueBetweenessCentrality = nx.betweenness_centrality(largestClique)
     secondLargestCliqueBetweenessCentrality = nx.betweenness_centrality(secondLargestClique)
     largestCliqueBottleneckUser = max(largestCliqueBetweenessCentrality, key=largestCliqueBetweenessCentrality.get)
@@ -49,38 +48,38 @@ class SocialNetwork:
 
   # findLargestCommunities can find the largest cliques
   # based on the shared interest
-  # (e.g Github Sponsor https://github.com/sponsors/explore)
+  # (e.g https://www.wired.com/story/facebook-people-you-may-know-friend-suggestions/ )
   def findLargestCommunities(self):
-    largestClique = set(sorted(nx.find_cliques(self.githubGraph), key=lambda x: len(x))[-1])
-    githubLargestClique = self.githubGraph.subgraph(largestClique).copy()
+    largestClique = set(sorted(nx.find_cliques(self.fbGraph), key=lambda x: len(x))[-1])
+    facebookLargestClique = self.fbGraph.subgraph(largestClique).copy()
     # Go out 1 degree of separation
-    for node in list(githubLargestClique.nodes()):
-        githubLargestClique.add_nodes_from(self.githubGraph.neighbors(node))
-        githubLargestClique.add_edges_from(zip([node]*len(list(self.githubGraph.neighbors(node))), self.githubGraph.neighbors(node)))
+    for node in list(facebookLargestClique.nodes()):
+        facebookLargestClique.add_nodes_from(self.fbGraph.neighbors(node))
+        facebookLargestClique.add_edges_from(zip([node]*len(list(self.fbGraph.neighbors(node))), self.fbGraph.neighbors(node)))
 
-    nodePosition = nx.spring_layout(githubLargestClique)
+    nodePosition = nx.spring_layout(facebookLargestClique)
     plt.figure(figsize=(8, 8))
-    plt.title("Largest Clique in the Graph")
-    nx.draw(githubLargestClique, nodePosition, with_labels=True)
+    plt.title("Largest Clique in the Facebook Graph")
+    nx.draw(facebookLargestClique, nodePosition, with_labels=True)
     plt.show(block = False)
     plt.pause(5)
 
-  # recommendedCollaborators will recommend the github users who collaborated to other collaborators in the past and
-  # but hasn't had any collaborations with their neighbors's collaborators
-  # by using BFS to determine if there is a shortest path between them
-  def recommendedCollaborators(self) -> list[tuple]:
-    recommendedCollaborators = defaultdict(int)
-    for node, _ in self.githubGraph.nodes(data=True):
+  # recommendedFriends will recommend friends who shared similar interest
+  # but hasn't been friends yet by creating a combination between the current user's neighbor
+  # If there is no connection, we will recommend them
+  def recommendedFriends(self) -> list[tuple]:
+    recommendedFriends = defaultdict(int)
+    for node, _ in self.fbGraph.nodes(data=True):
         # Create a combination of 2 with all the neighbor nodes that the node has
         # and determine if there is a path between them. If not,
         # add them
-        for firstNeighborCollab, secondNeighborCollab  in combinations(self.githubGraph.neighbors(node), 2):
-            if not self.pathExistBFS(firstNeighborCollab, secondNeighborCollab):
-                recommendedCollaborators[(firstNeighborCollab, secondNeighborCollab)] += 1
+        for firstNeighborFriend, secondNeighborFriend  in combinations(self.fbGraph.neighbors(node), 2):
+            if not self.pathExistBFS(firstNeighborFriend, secondNeighborFriend):
+                recommendedFriends[(firstNeighborFriend, secondNeighborFriend)] += 1
 
     # Identify the top 10 pairs of users
-    sortedRecommendedCollaborators = sorted(recommendedCollaborators.values())
-    top10Pairs = [pair for pair, count in recommendedCollaborators.items() if count > sortedRecommendedCollaborators[-10]]
+    sortedRecommendedFriends = sorted(recommendedFriends.values())
+    top10Pairs = [pair for pair, count in recommendedFriends.items() if count > sortedRecommendedFriends[-10]]
     return top10Pairs
 
   # pathExistBFS will use breath first search
@@ -97,7 +96,7 @@ class SocialNetwork:
     # Traverse all its adjacent node
     for node in queue:
         # Once all adjacent are visited, then their adjacent are traversed
-        neighbors = self.githubGraph.neighbors(node)
+        neighbors = self.fbGraph.neighbors(node)
         if endVertex in neighbors:
             return True
         else:
@@ -113,29 +112,29 @@ class SocialNetwork:
 def createSocialNet() -> SocialNetwork:
   dataPath = downloadDataset()
   with open(dataPath[1], 'rb') as f:
-    githubGraph = pickle.load(f)
+    fbGraph = pickle.load(f)
 
   # Since pilotlib has a problem with performance for over than 2000 nodes with the corresponding edges
-  # We will only take the 150 nodes which have the highest degree (nodes that have most edges)
-  # and simulate a subgraph of github
-  githubNodeDegree = dict(githubGraph.degree())
-  sortedNodes = sorted(githubNodeDegree.items(), key=lambda x: x[1], reverse=True)
+  # We will only take the 200 nodes which have the highest degree (nodes that have most edges)
+  # and simulate a subgraph of fb
+  fbNodeDegree = dict(fbGraph.degree())
+  sortedNodes = sorted(fbNodeDegree.items(), key=lambda x: x[1], reverse=True)
   topDegreeNodes = [node for node, _ in sortedNodes[:200]]
-  githubSubGraph = githubGraph.subgraph(topDegreeNodes)
-  print("Number of nodes", len(githubSubGraph.nodes()))
-  print("Number of edges", len(githubSubGraph.edges()))
-  print("Number of nodes", githubSubGraph.nodes())
-  sn = SocialNetwork(githubSubGraph)
+  fbSubGraph = fbGraph.subgraph(topDegreeNodes)
+  print("Number of nodes", len(fbSubGraph.nodes()))
+  print("Number of edges", len(fbSubGraph.edges()))
+  print("Number of nodes", fbSubGraph.nodes())
+  sn = SocialNetwork(fbSubGraph)
   # sn.drawGraph()
   return sn
 
 
 def socialNetworkAnalysis() -> None:
   sn = createSocialNet()
-  importantColaborators = sn.findImportantCollaborators()
-  print("The most prolific colaborators", importantColaborators)
+  importantPeople = sn.findImportantPeople()
+  print("The most prolific people", importantPeople)
   #sn.drawGraph(degreeDistribution=True)
   #sn.findLargestCommunities()
   sn.connectCommunities()
-  recommendedCollaborators = sn.recommendedCollaborators()
-  print("Top 10 recommended collaborators:", recommendedCollaborators)
+  recommendedFriends = sn.recommendedFriends()
+  print("Top 10 recommended friends:", recommendedFriends)
